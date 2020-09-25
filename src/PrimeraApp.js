981b9ffa8f9f9ce32ef46d7f1eeeda25d1959822
mdCarron <mdcarron.dev@gmail.com>
2020-09-25 18:36:05 -0300

    Completa 9. Retornar elementos en el Componente - Fragment

---
+++ src/PrimeraApp.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+const PrimeraApp = () => {
+  return (
+    <Fragment>
+      <h1>Hola Mundo!</h1>
+      <p>Mi primera aplicación React</p>
+    </Fragment>
+  );
+};
+
+export default PrimeraApp;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import PrimeraApp from "./PrimeraApp";
+import "./index.css";
 
-const saludo = <h1>Hola Mundo!</h1>;
 const divRoot = document.querySelector("#root");
 
-ReactDOM.render(saludo, divRoot);
+ReactDOM.render(<PrimeraApp />, divRoot);
