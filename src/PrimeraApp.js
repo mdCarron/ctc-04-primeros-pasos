b6ee77d40f8bdae8b75b8011bcb18cf0f325a7c1
mdCarron <mdcarron.dev@gmail.com>
2020-09-26 21:20:56 -0300

    Completa 13. DefaultProps

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,17 +1,20 @@
 import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 
-const PrimeraApp = ({ saludo }) => {
+const PrimeraApp = ({ saludo, subtitulo }) => {
   return (
     <Fragment>
       <h1>{saludo}</h1>
-      <p>Mi primera aplicación React</p>
+      <p>{subtitulo}</p>
     </Fragment>
   );
 };
 
 PrimeraApp.propTypes = {
   saludo: PropTypes.string.isRequired,
+};
+PrimeraApp.defaultProps = {
+  subtitulo: "Este es el subtitulo",
 };
 
 export default PrimeraApp;
