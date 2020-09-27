6a67e9e5b0a2f2205bc4880c9cd1be753ec70095
mdCarron <mdcarron.dev@gmail.com>
2020-09-26 22:03:01 -0300

    Completa 15. Evento click (Eventos en general)

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,10 +2,15 @@ import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
+  const handleAdd = (e) => {
+    console.log(e);
+  };
+
   return (
     <Fragment>
       <h1>CounterApp</h1>
       <h2>{value}</h2>
+      <button onClick={handleAdd}>+1</button>
     </Fragment>
   );
 };
